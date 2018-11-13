8b48e7ef82c8fcdab6078c33e5efe0c65aeb5d50
Nicolette <nicolette.pribanic@avl.com>
2018-11-13 19:28:21 +0100

    Make exercise 02 compile

--- exercise/02/array.cpp
+++ exercise/02/array.cpp
@@ -1,1 +1,19 @@
 #include "array.h"
+
+array::array()
+{
+}
+
+array::array(int size, double value)
+{
+}
+
+int array::size() const
+{
+	return 0;
+}
+
+double array::at(int index) const
+{
+	return 0.0;
+}
--- exercise/02/array.h
+++ exercise/02/array.h
@@ -4,4 +4,8 @@ class array
 	double* p;
 	int n;
 public:
+	array();
+	array(int size, double value);
+	int size() const;
+	double at(int index) const;
 };
